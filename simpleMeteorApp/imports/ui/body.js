bfe41b69757b7bb813124dd2c88a607f038fe189
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-22 18:19:50 -0300

    Simple logic to add new tasks

--- simpleMeteorApp/imports/ui/body.js
+++ simpleMeteorApp/imports/ui/body.js
@@ -5,7 +5,27 @@ import { Tasks } from '../api/tasks.js';
 
 
 Template.body.helpers({
-    tasks(){
+    tasks() {
         return Tasks.find({});
     },
 })
+
+Template.body.events({
+    'submit .new-task'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        // Insert a task into the collection
+        Tasks.insert({
+            text,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        target.text.value = '';
+    },
+});
