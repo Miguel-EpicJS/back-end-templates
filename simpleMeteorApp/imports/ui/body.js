56631792e9b56bf9d506dae8477c370ba6646cf7
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-23 11:50:58 -0300

    I miss some important code

--- simpleMeteorApp/imports/ui/body.js
+++ simpleMeteorApp/imports/ui/body.js
@@ -2,30 +2,30 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
-import './task.js';
-import './body.html';
 import { Tasks } from '../api/tasks.js';
 
+import './task.js';
+import './body.html';
 
 Template.body.onCreated(function bodyOnCreated() {
     this.state = new ReactiveDict();
     Meteor.subscribe('tasks');
-})
+});
 
 Template.body.helpers({
     tasks() {
-
         const instance = Template.instance();
         if (instance.state.get('hideCompleted')) {
+            // If hide completed is checked, filter tasks
             return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
         }
-
+        // Otherwise, return all of the tasks
         return Tasks.find({}, { sort: { createdAt: -1 } });
     },
     incompleteCount() {
         return Tasks.find({ checked: { $ne: true } }).count();
     },
-})
+});
 
 Template.body.events({
     'submit .new-task'(event) {
@@ -42,7 +42,7 @@ Template.body.events({
         // Clear form
         target.text.value = '';
     },
-    'change .hide-completed input'(event, instace) {
-        instace.state.set('hideCompleted', event.target.checked);
-    }
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    },
 });
--- simpleMeteorApp/imports/ui/task.js
+++ simpleMeteorApp/imports/ui/task.js
@@ -1,9 +1,8 @@
-import { Template } from 'meteor/templating';
+
 import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
 
-import { Tasks } from '../api/tasks.js';
-
-import './task.html'
+import './task.html';
 
 Template.task.helpers({
     isOwner() {
@@ -11,15 +10,17 @@ Template.task.helpers({
     },
 });
 
-
 Template.task.events({
     'click .toggle-checked'() {
+        // Set the checked property to the opposite of its current value
         Meteor.call('tasks.setChecked', this._id, !this.checked);
+
+
     },
     'click .delete'() {
         Meteor.call('tasks.remove', this._id);
     },
     'click .toggle-private'() {
         Meteor.call('tasks.setPrivate', this._id, !this.private);
-    }
-})
+    },
+});
