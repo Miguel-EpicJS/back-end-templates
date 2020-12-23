1b442576b6ed97d5621b54466956be6147716cfb
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-23 11:23:23 -0300

    Call the tasks.setChecked and tasks.remove

--- simpleMeteorApp/imports/ui/task.js
+++ simpleMeteorApp/imports/ui/task.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
 
 import { Tasks } from '../api/tasks.js';
 
@@ -6,11 +7,9 @@ import './task.html'
 
 Template.task.events({
     'click .toggle-checked'() {
-        Tasks.update(this._id, {
-            $set: { checked: ! this.checked },
-        });
+        Meteor.call('tasks.setChecked', this._id, !this.checked);
     },
     'click .delete'(){
-        Tasks.remove(this._id);
+        Meteor.call('tasks.remove', this._id);
     }
 })
